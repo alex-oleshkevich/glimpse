5a730d8f0f09afd2300f77806fe638bdc90503b0
alex.oleshkevich <alex.oleshkevich@gmail.com>
2026-08-31 13:21:07 +0200

    Move clock indicator to submodule

---
+++ crates/glimpse-panel/src/applets/clock/popover.rs
@@ -0,0 +1,40 @@
+use relm4::{
+    ComponentParts, ComponentSender, SimpleComponent,
+    gtk::{self, prelude::*},
+};
+
+pub struct Popover {}
+
+#[derive(Debug)]
+pub struct Init {}
+
+#[derive(Debug)]
+pub enum Input {}
+
+#[relm4::component(pub)]
+impl SimpleComponent for Popover {
+    type Init = Init;
+    type Input = Input;
+    type Output = ();
+
+    view! {
+        root = gtk::Box {
+            set_orientation: gtk::Orientation::Horizontal,
+            set_spacing: 4,
+        }
+    }
+
+    fn init(
+        init: Self::Init,
+        _root: Self::Root,
+        _sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let model = Popover {};
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, message: Self::PopoverInput, _sender: ComponentSender<Self>) {
+        match message {}
+    }
+}
